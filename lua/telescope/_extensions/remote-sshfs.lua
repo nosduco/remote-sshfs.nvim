22ab0903640cbc495b62ec1521923a97caf148af
Tony Duco <nosduco@gmail.com>
2023-03-25 13:11:13 -0400

    functional connection preview in telescope

--- lua/telescope/_extensions/remote-sshfs.lua
+++ lua/telescope/_extensions/remote-sshfs.lua
@@ -1,5 +1,25 @@
 local sorters = require "telescope.sorters"
 local state = require "telescope.actions.state"
+local previewers = require "telescope.previewers"
+
+local function build_host_preview(hosts, name)
+  if name == "" or nil then
+    return {}
+  end
+
+  local lines = {}
+  local host = hosts[name]
+
+  table.insert(lines, "Host " .. host["HostName"])
+  for key, value in pairs(host) do
+    if key ~= "name" then
+      table.insert(lines, string.format("\t%s %s", key, value))
+    end
+  end
+  table.insert(lines, "")
+
+  return lines
+end
 
 local function connect(_)
   local pickers, finders, actions
@@ -14,9 +34,18 @@ local function connect(_)
   local local_connections = require "remote-sshfs.connections"
   local hosts = local_connections.list_hosts()
 
+  local previewer = previewers.new_buffer_previewer {
+    define_preview = function(self, entry)
+      local lines = build_host_preview(hosts, entry.value)
+      vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+      require("telescope.previewers.utils").highlighter(self.state.bufnr, "sshconfig")
+    end,
+  }
+
   pickers
     .new(_, {
       prompt_title = "Connect to remote host",
+      previewer = previewer,
       finder = finders.new_table {
         results = vim.tbl_keys(hosts),
       },
